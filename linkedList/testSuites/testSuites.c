fb420c9bdfb85ce05956d460a97b1c92d163b562
ketherball <121590283+ketherball@users.noreply.github.com>
2024-10-17 14:29:36 -0400

    tests: implement abstract linked list tests for all typedef'd linked list structs

--- linkedList/testSuites/testSuites.c
+++ linkedList/testSuites/testSuites.c
@@ -8,8 +8,6 @@ void linkedList_popTests();
 void linkedList_shiftTests();
 void linkedList_unshiftTests();
 
-void fillList(LinkedList* plist, int amount);
-
 void linkedList_testSuites() {
     printf("=====LINKED LIST TESTS=====\n\n");
     suite("Access:", linkedList_accessTests);
@@ -24,128 +22,35 @@ void linkedList_testSuites() {
 
 void linkedList_accessTests() {
     LinkedList list;
-    linkedList_init(&list);
-    fillList(&list, 9);
-
-    int val = linkedList_access(&list, 4);
-
-    it("Accesses accurate index", val == 5);
-
-    int headVal = list.head ? list.head->val : 0;
-    it("Returns head if idx is less than zero", headVal == 1);
-
-    int tailVal = list.tail ? list.tail->val : 0;
-    it("Returns tail if idx is greater than count", tailVal == 9);
-
-    linkedList_reset(&list);
+    abstract_linkedList_accessTests(&list, linkedList_access);
 }
 
 void linkedList_addTests() {
     LinkedList list;
-    linkedList_init(&list);
-    fillList(&list, 9);
-
-    linkedList_add(&list, 6, 2);
-    int val = linkedList_access(&list, 6);
-
-    it("Adds to accurate index", val == 2);
-    it("Updates count", list.count == 10);
-
-    linkedList_add(&list, -5, -6);
-    int headVal = list.head ? list.head->val : 0;
-    it("Adds to head if index is less than zero", headVal == -6);
-
-    linkedList_add(&list, 15, 6);
-    int tailVal = list.tail ? list.tail->val : 0;
-    it("Adds to tail if index is greater than count", tailVal == 6);
-
-    linkedList_reset(&list);
+    abstract_linkedList_addTests(&list, linkedList_add);
 }
 
 void linkedList_removeTests() {
     LinkedList list;
-    linkedList_init(&list);
-    fillList(&list, 9);
-
-    int val = linkedList_remove(&list, 3);
-
-    it("Removes from accurate index", val == 4);
-    it("Updates count", list.count == 8);
-
-    int headVal = list.head ? list.head->val : 0;
-    it("Removes head if index is less than zero", linkedList_remove(&list, -5) == headVal);
-
-    int tailVal = list.tail ? list.tail->val : 0;
-    it("Removes tail if index is less than zero", linkedList_remove(&list, 13) == tailVal);
-
-    linkedList_reset(&list);
+    abstract_linkedList_removeTests(&list, linkedList_remove);
 }
 
 void linkedList_pushTests() {
     LinkedList list;
-    linkedList_init(&list);
-
-    linkedList_push(&list, 1);
-    linkedList_push(&list, 2);
-    linkedList_push(&list, 3);
-    linkedList_push(&list, 4);
-    linkedList_push(&list, 5);
-    linkedList_push(&list, 6);
-    linkedList_push(&list, 7);
-    linkedList_push(&list, 8);
-    linkedList_push(&list, 9);
-
-    int tailVal = list.tail ? list.tail->val : 0;
-    it("Adds to end of list", tailVal == 9);
-    it("Updates count", list.count == 9);
-
-    linkedList_reset(&list);
+    abstract_linkedList_pushTests(&list, linkedList_push);
 }
 
 void linkedList_popTests() {
     LinkedList list;
-    linkedList_init(&list);
-    fillList(&list, 9);
-
-    int poppedVal = linkedList_pop(&list);
-    it("Removes last node", poppedVal == 9);
-    it("Updates count", list.count == 8);
-
-    linkedList_reset(&list);
+    abstract_linkedList_popTests(&list, linkedList_pop);
 }
 
 void linkedList_shiftTests() {
     LinkedList list;
-    linkedList_init(&list);
-    fillList(&list, 9);
-
-    linkedList_shift(&list, 0);
-
-    int headVal = list.head ? list.head->val : -1;
-    it("Adds to beginning", headVal == 0);
-    it("Updates count", list.count == 10);
-
-    linkedList_reset(&list);
+    abstract_linkedList_shiftTests(&list, linkedList_shift);
 }
 
 void linkedList_unshiftTests() {
     LinkedList list;
-    linkedList_init(&list);
-    fillList(&list, 9);
-
-    int val = linkedList_unshift(&list);
-
-    int headVal = list.head ? list.head->val : 0;
-    it("Removes first node", val == 1);
-    it("Updates head", headVal == 2);
-    it("Updates count", list.count == 8);
-
-    linkedList_reset(&list);
-}
-
-// UTILS
-void fillList(LinkedList* plist, int amount) {
-    for (int i = 0; i < amount; i++) {
-        linkedList_push(plist, i+1);
-    }
+    abstract_linkedList_unshiftTests(&list, linkedList_unshift);
 }
---
+++ linkedList/testSuites/tests/tests.h
@@ -0,0 +1,18 @@
+#ifndef c_dsalgos_linkedlist_testsuites_tests_tests_h
+#define c_dsalgos_linkedlist_testsuites_tests_tests_h
+
+#include "../../../common.h"
+#include "../../linkedList.h"
+
+void abstract_linkedList_accessTests(LinkedList* plist, int (*linkedList_access)(LinkedList* plist, int idx));
+
+void abstract_linkedList_addTests(LinkedList* plist, void (*linkedList_add)(LinkedList* plist, int idx, int val));
+void abstract_linkedList_removeTests(LinkedList* plist, int (*linkedList_remove)(LinkedList* plist, int idx));
+
+void abstract_linkedList_pushTests(LinkedList* plist, void (*linkedList_push)(LinkedList* plist, int val));
+void abstract_linkedList_popTests(LinkedList* plist, int (*linkedList_pop)(LinkedList* plist));
+
+void abstract_linkedList_shiftTests(LinkedList* plist, void (*linkedList_shift)(LinkedList* plist, int val));
+void abstract_linkedList_unshiftTests(LinkedList* plist, int (*linkedList_unshift)(LinkedList* plist));
+
+#endif
