0b21ba04e2aa04d5e979b545d5f3da3c0de3d6e0
ketherball <121590283+ketherball@users.noreply.github.com>
2024-10-22 14:37:17 -0400

    feat: add menu

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include "common.h"
 #include "testSuites/testSuites.h"
+#include "__utils__/arrContains/arrContains.h"
+#include "__utils__/trimString/trimString.h"
+
+void menu();
 
 int main(int argc, char* argv[]) {
-    if (argsContainsTest(argc, argv)) runTestSuites(argc, argv);
+    if (arrContains(argc, argv, "test")) {
+        runTestSuites(argc, argv);
+    } else {
+        menu();
+    }
 
     return 1;
+}
+
+void menu() {
+    printf("\nWelcome to Data Structures.\n\n");
+    printf("Which data structure would you like to use?\n");
+    printf("    1. Linked List\n");
+    printf("    2. Stack\n");
+    printf("    3. Queue\n");
+    printf("    4. Binary Search Tree\n");
+    printf("\n");
+
+    char line[256];
+
+    if (fgets(line, sizeof line, stdin) != NULL) {
+        trimString(line);
+        if (
+            strcmp(line, "Linked List") == 0 ||
+            strcmp(line, "1") == 0 ||
+            strcmp(line, "ll") == 0
+        ) printf("ll\n");
+        if (
+            strcmp(line, "Stack") == 0 ||
+            strcmp(line, "2") == 0
+        ) printf("s\n");
+        if (
+            strcmp(line, "Queue") == 0 ||
+            strcmp(line, "3") == 0
+        ) printf("q\n");
+                if (
+            strcmp(line, "Binary Search Tree") == 0 ||
+            strcmp(line, "4") == 0 ||
+            strcmp(line, "bst") == 0
+        ) printf("bst\n");
+    }
 }
